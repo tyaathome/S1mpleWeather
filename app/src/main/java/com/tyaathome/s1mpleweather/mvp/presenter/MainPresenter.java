4fb37c4285f697ae8efa1a3789db3e2e2df8e5a8
tyaathome <tyaathome@163.com>
2018-05-29 11:31:53 +0800

    commit

--- app/src/main/java/com/tyaathome/s1mpleweather/mvp/presenter/MainPresenter.java
+++ app/src/main/java/com/tyaathome/s1mpleweather/mvp/presenter/MainPresenter.java
@@ -4,10 +4,11 @@ import android.content.Context;
 import android.graphics.drawable.BitmapDrawable;
 import android.text.TextUtils;
 
-import com.tyaathome.s1mpleweather.model.bean.main.sstq.SstqBean;
+import com.tyaathome.s1mpleweather.model.bean.main.weekweather.WeekWeatherBean;
+import com.tyaathome.s1mpleweather.model.bean.main.weekweather.WeekWeatherInfoBean;
 import com.tyaathome.s1mpleweather.mvp.base.BaseView;
 import com.tyaathome.s1mpleweather.mvp.contract.MainContract;
-import com.tyaathome.s1mpleweather.net.pack.main.sstq.SstqPackUp;
+import com.tyaathome.s1mpleweather.net.pack.main.week.WeekWeatherPackUp;
 import com.tyaathome.s1mpleweather.utils.CommonUtils;
 
 import java.util.ArrayList;
@@ -73,11 +74,14 @@ public class MainPresenter implements MainContract.Presenter {
                 .throttleWithTimeout(500, TimeUnit.MILLISECONDS)
                 .observeOn(Schedulers.io())
                 .map(s -> {
-                    SstqPackUp up = new SstqPackUp(s);
-                    SstqBean bean = up.getCacheData(Realm.getDefaultInstance());
-                    if(bean != null && bean.getSstq() != null) {
-                        String ico = bean.getSstq().getWt_daytime_ico();
-                        return CommonUtils.getWeatherBackground(mContext, ico);
+                    WeekWeatherPackUp up = new WeekWeatherPackUp(s);
+                    WeekWeatherBean bean = up.getCacheData(Realm.getDefaultInstance());
+                    if(bean != null && bean.getWeek() != null && bean.getWeek().size() > 1) {
+                        WeekWeatherInfoBean infoBean =  bean.getWeek().get(1);
+                        if(infoBean != null && !TextUtils.isEmpty(infoBean.getWd_day_ico())) {
+                            String ico = infoBean.getWd_day_ico();
+                            return CommonUtils.getWeatherBackground(mContext, ico);
+                        }
                     }
                     return null;
                 })
